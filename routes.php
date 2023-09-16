18b73edaa0d980de8871ab5858e48152016ea639
dimti <demidov@dimti.ru>
2023-09-16 18:59:55 +0300

    * change default api route for jwtauth to jwtauth, because api use on other plugins * use Lcobucci jwt auth provider

--- routes.php
+++ routes.php
@@ -3,7 +3,7 @@
 use Backend\Facades\Backend;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'api'], function () {
+Route::group(['prefix' => 'jwtauth'], function () {
     Route::post('login', ['uses' => '\Vdomah\JWTAuth\Controllers\AuthController@login']);
     Route::group(['middleware' => 'jwt.auth'], function () {
         Route::post('refresh', ['uses' => '\Vdomah\JWTAuth\Controllers\AuthController@refresh']);
