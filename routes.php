562d8c6a3537b9d97cc9afc25ccdfc251d480c59
dimti <demidov@dimti.ru>
2023-10-08 09:04:54 +0300

    * use static class names in jwtauth routes

--- routes.php
+++ routes.php
@@ -2,23 +2,25 @@
 
 use Backend\Facades\Backend;
 use Illuminate\Support\Facades\Route;
+use Vdomah\JWTAuth\Controllers\AuthController;
+use Vdomah\JWTAuth\Controllers\RefreshController;
 
 Route::group(['prefix' => 'jwtauth'], function () {
-    Route::post('login', ['uses' => '\Vdomah\JWTAuth\Controllers\AuthController@login']);
-    Route::post('refresh', ['uses' => '\Vdomah\JWTAuth\Controllers\RefreshController@refresh']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('refresh', [RefreshController::class, 'refresh']);
     Route::group(['middleware' => 'jwt.auth'], function () {
-        Route::post('invalidate', ['uses' => '\Vdomah\JWTAuth\Controllers\AuthController@invalidate']);
-        Route::post('signup', ['uses' => '\Vdomah\JWTAuth\Controllers\AuthController@signup']);
-        Route::get('user', ['uses' => '\Vdomah\JWTAuth\Controllers\AuthController@user']);
+        Route::post('invalidate', [AuthController::class, 'invalidate']);
+        Route::post('signup', [AuthController::class, 'signup']);
+        Route::get('user', [AuthController::class, 'user']);
     });
 });
 
 Route::group(['prefix' => Backend::baseUrl() . '/jwtauth'], function () {
-    Route::post('login', ['uses' => '\Vdomah\JWTAuth\Controllers\AuthController@login']);
+    Route::post('login', [AuthController::class, 'login']);
     Route::group(['middleware' => 'jwt.auth'], function () {
         Route::post('refresh', ['uses' => '\Vdomah\JWTAuth\Controllers\AuthController@refresh']);
-        Route::post('invalidate', ['uses' => '\Vdomah\JWTAuth\Controllers\AuthController@invalidate']);
-        Route::get('user', ['uses' => '\Vdomah\JWTAuth\Controllers\AuthController@user']);
+        Route::post('invalidate', [AuthController::class, 'invalidate']);
+        Route::get('user', [AuthController::class, 'user']);
     });
 });
 
